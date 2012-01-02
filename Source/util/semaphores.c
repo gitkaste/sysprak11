0c0e7c2b72066041774087ce8013f1c44e4f1918
Max Klinger <max@fs.lmu.de>
2012-01-02 13:04:09 +0100

    fixed semaphores

--- Source/util/semaphores.c
+++ Source/util/semaphores.c
@@ -5,17 +5,21 @@
 
 int semWait(int semid, int semnum){
 	struct sembuf sbuf;
-	sbuf.semnum = semnum;
+	sbuf.sem_num = semnum;
 	sbuf.sem_op = -1;
 	return semop(semid, &sbuf, 1);
 }
 int semSignal(int semid, int semnum){
 	struct sembuf sbuf;
-	sbuf.semnum = semnum;
+	sbuf.sem_num = semnum;
 	sbuf.sem_op = 1;
 	return semop(semid, &sbuf, 1);
 }
 int semVal(int semid, int semnum){
-	union semun smun;
-	return semctl(semid, semnum, GETVAL,smun);
+	union semun {
+		int val; 
+		struct semid_ds *buf; 
+		ushort *array;
+	} smun;
+	return semctl(semid, semnum, GETVAL, smun);
 }
