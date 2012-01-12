1301a169e4c668f9e3e68d8a4d1a4df4b90ffef9
Max Klinger <max@fs.lmu.de>
2012-01-12 02:45:37 +0100

    process utils (cpa)

---
+++ Source/util/childprocesses.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <signal.h>
+#include "util.h"
+
+struct array *addChildProcess(struct array *cpa, unsigned char type, pid_t pid){
+	struct child * c = malloc(sizeof(struct processChild));
+	c->type = type;
+	c->pid = pid;
+	/* I don't need to be careful here because the caller needs to take care of not losing cpa */
+	cpa = addArrayItem(cpa, c);
+	free(c);
+	return cpa;
+}
+
+int remChildProcess(struct array *cpa, pid t pid){
+	struct child *c;
+	long unsigned int i = 0;
+	while ( (c = iterateArray(cpa, &i)) ){
+		if (pid == c->pid){
+			if ( -1 == remArrayItem(cpa, i-1)){
+				fprintf("couldn't remove child entry");
+				return -1;
+				else return 1;
+			}
+		}
+	}
+}
+
+/* Why don't we take advantage of process groups */
+int sendSignalToChildren(struct array *cpa, unsigned char type, int sig){
+	struct child *c;
+	long unsigned int i = 0;
+	while ( (c = iterateArray(cpa, &i)) ){
+		if (c->type == type){
+			if ( kill (c->pid, sig) ){
+				perror("(sendSignalToChildren)");
+				return -1;
+			}
+		}
+	}
+	return 1;
+}
+
+
